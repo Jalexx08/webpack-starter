f7ed3fd24df0ecb68296621e913d723637191ef3
Jalexx <Jhalx2198@gmail.com>
2020-04-14 09:24:35 -0500

    Cambiando mensaje

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -5,7 +5,7 @@ import '../css/componentes.css';
 export const saludar = (nombre) => {
     console.log('Creando etiqueta h1');
     const h1 = document.createElement('h1');
-    h1.innerHTML = ` Hola, ${nombre}, ¿cómo estás?`;
+    h1.innerHTML = ` ${nombre}, Configuración inicial`;
 
     document.body.append(h1);
 }
